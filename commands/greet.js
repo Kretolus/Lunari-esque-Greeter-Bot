baf8345cb55100980e788135c1a8404940d2db79
lkrzyzanski <lukasz.krzyzansk@gmail.com>
2018-03-06 20:04:40 +0100

    Refactored the greet command slightly.

--- commands/greet.js
+++ commands/greet.js
@@ -12,16 +12,17 @@ module.exports = {
                 .then((member) => {
                     client.user.setAvatar('./icons/general_kenobi.png')
                         .then((user) => {
-                            message.channel.send('Hello there!');
-                            member.setNickname('Lunari-esque Greeter Bot')
-                                .then((member) => {})
-                                .catch(logger.log);
-                            user.setAvatar('./icons/bot-img.png')
-                                .then((user) => {})
-                                .catch(logger.log);
+                            message.channel.send('Hello there!').then(() => {
+                                member.setNickname('Lunari-esque Greeter Bot')
+                                    .then((member) => {})
+                                    .catch(logger.error);
+                                user.setAvatar('./icons/bot-img.png')
+                                    .then((user) => {})
+                                    .catch(logger.error);
+                            }).catch(logger.error);
                         })
-                        .catch(logger.log);
-                }).catch(logger.log);
+                        .catch(logger.error);
+                }).catch(logger.error);
         });
     },
 };
